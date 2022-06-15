84cfe5773029b5aaec41ccd27f7193b790a6cedd
SongeDaiDai <89518368+GhastNier@users.noreply.github.com>
2022-06-15 17:08:20 -0400

    First Commit

---
+++ app/src/main/java/com/example/myrestaurant/CartTabActivity.java
@@ -0,0 +1,141 @@
+package com.example.myrestaurant;
+
+import static com.example.myrestaurant.CartLists.TabContent.ITEMS;
+import static com.example.myrestaurant.CartLists.TabContent.initData;
+import static com.example.myrestaurant.GetterAndSetter.getItemNumber;
+import static com.example.myrestaurant.GetterAndSetter.getTotalValue;
+import static com.example.myrestaurant.GetterAndSetter.setItemNumber;
+import static com.example.myrestaurant.GetterAndSetter.setTotalValue;
+import static com.example.myrestaurant.MainActivity.items;
+import static com.example.myrestaurant.dialog.AddItemDialog.df;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.myrestaurant.CartLists.CartTab;
+import com.example.myrestaurant.CartLists.ItemTab;
+import com.example.myrestaurant.CartLists.SectionsPagerAdapter;
+import com.example.myrestaurant.CartLists.TabContent;
+import com.example.myrestaurant.databinding.ActivityCartTabBinding;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class CartTabActivity extends AppCompatActivity {
+    private final int[] i = {1};
+    private ActivityCartTabBinding binding;
+    private SectionsPagerAdapter fragPager;
+    private NavController navController;
+    TabLayout tabs;
+    ViewPager viewPager;
+    ExtendedFloatingActionButton main;
+    FloatingActionButton clear, home;
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setBinding();
+        setButton();
+        setValue();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_cart_tab);
+        fragPager.addFragment(ItemTab.newInstance(1), "Items");
+        fragPager.addFragment(CartTab.newInstance(1), "Cart");
+        viewPager.setAdapter(fragPager);
+        tabs.setupWithViewPager(viewPager);
+        tabs.getTabAt(0).setIcon(R.drawable.ic_list);
+        tabs.getTabAt(1).setIcon(R.drawable.ic_cart);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        navController.navigate(R.id.action_global_mainActivity);
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+    }
+
+    private void setBinding() {
+        binding = ActivityCartTabBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.myfab2.shrink();
+        initData();
+        fragPager = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        tabs = binding.tabs;
+        viewPager = binding.viewPager;
+    }
+
+    public void setValue() {
+        StringBuilder builder = new StringBuilder();
+        main.setText(builder.append(df.format(getTotalValue())).append(getString(R.string.dollar)).toString());
+    }
+
+
+    public void clearButton(View view) {
+        clearList();
+        fragPager = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        tabs = binding.tabs;
+        viewPager = binding.viewPager;
+        fragPager.addFragment(ItemTab.newInstance(1), "Items");
+        fragPager.addFragment(CartTab.newInstance(1), "Cart");
+        viewPager.setAdapter(fragPager);
+        tabs.setupWithViewPager(viewPager);
+        tabs.getTabAt(0).setIcon(R.drawable.ic_list);
+        tabs.getTabAt(1).setIcon(R.drawable.ic_cart);
+
+    }
+
+    public void toHome(View view) {
+        navController.navigate(R.id.action_global_mainActivity);
+    }
+
+    private void setButton() {
+        main = binding.myfab2;
+        clear = binding.clearcartfab;
+        home = binding.toHomeFab;
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (main.isExtended()) {
+                    main.shrink();
+                    return;
+                } else main.extend();
+                return;
+            }
+        });
+    }
+
+    public void clearList() {
+        if (getItemNumber() > 0) {
+            items.clear();
+            ITEMS.clear();
+            setTotalValue(0.00);
+            setItemNumber(1);
+            main.setText("0.00$");
+            TabContent.resetCartItemMap();
+            Toast.makeText(this, R.string.clear_cart, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "The cart was already empty", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/example/myrestaurant/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.myrestaurant;
+
+import static com.example.myrestaurant.GetterAndSetter.getItemNumber;
+import static com.example.myrestaurant.GetterAndSetter.getTotalValue;
+import static com.example.myrestaurant.GetterAndSetter.setItemNumber;
+import static com.example.myrestaurant.GetterAndSetter.setLargeLayout;
+import static com.example.myrestaurant.GetterAndSetter.setTotalValue;
+import static com.example.myrestaurant.dialog.AddItemDialog.df;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myrestaurant.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.MultimapBuilder;
+
+public class MainActivity extends AppCompatActivity {
+    public static ListMultimap<String, String> items;
+    public static final String cartKey = "cartKey";
+    public static final String valueKey = "valueKey";
+    private NavController navController;
+    public static AppBarConfiguration appBarConfiguration;
+    public static SharedPreferences cart, value;
+
+    private ActivityMainBinding binding;
+    private TextView text1;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        text1 = (TextView) findViewById(R.id.cart_amount);
+        onCartChange();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createBinding();
+        text1 = (TextView) findViewById(R.id.cart_amount);
+        createCartSharedPref();
+    }
+    public void onCartChange() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(getString(R.string.cart_val_void))
+                .append(" ")
+                .append(df.format(getTotalValue()))
+                .append(getString(R.string.dollar));
+        text1.setText(builder.toString());
+    }
+
+    public void clearList() {
+        if (getItemNumber() > 1) {
+            setTotalValue(0.00);
+            onCartChange();
+            setItemNumber(1);
+            Toast.makeText(text1.getContext(), R.string.clear_cart, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(text1.getContext(), "The cart was already empty", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createCartSharedPref() {
+        items = MultimapBuilder.treeKeys().arrayListValues().build();
+        cart = getSharedPreferences(cartKey, Context.MODE_PRIVATE);
+        cart.edit().putString(cartKey, "0").apply();
+    }
+
+    private void createBinding() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(findViewById(R.id.toolbar), navController, appBarConfiguration);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.my_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_global_cartTabActivity);
+            }
+        });
+        setLargeLayout(getResources().getBoolean(R.bool.large_layout));
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
+
